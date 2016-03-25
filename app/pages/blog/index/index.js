c7515846a5f50a32215cee8e0005f6508cb0b30c
wincy7 <akane_sisi@yahoo.co.jp>
2016-03-25 17:20:16 +0900

    画面調整

--- app/pages/blog/index/index.js
+++ app/pages/blog/index/index.js
@@ -14,12 +14,12 @@ import {DetailPage} from '../detail/detail';
 */
 @Page({
     templateUrl: 'build/pages/blog/index/index.html',
-    providers: [BlogService],
+    providers: [BlogService,UserService],
     pipes: [TranslatePipe]
 })
 export class BlogIndexPage {
     static get parameters() {
-        return [[NavController], [BlogService]];
+        return [[NavController], [BlogService], [UserService]];
     }
 
     constructor(nav, blogService) {
@@ -30,8 +30,34 @@ export class BlogIndexPage {
         //     this.blogs = data;
         // });
         this.blogService.getCommunityListForTop().then(data => {
+            data.forEach(function(element) {
+                var publishDate = new Date(element.publishStartDate.replace("T", " "));
+                var now = new Date();
+                var publishedMinutes = parseInt((now.getTime() - publishDate.getTime()) / (60 * 1000));
+                if (publishedMinutes >= 60 * 24 * 365) {
+                    // 一年前の場合
+                    var years = parseInt(publishedMinutes / (60 * 24 * 365));
+                    element.publishStartDate = years + "年前";
+                } else if (publishedMinutes >= 60 * 24 * 30) {
+                    // 一か月前の場合
+                    var months = parseInt(publishedMinutes / (60 * 24 * 30));
+                    element.publishStartDate = months + "月前";
+                } else if (publishedMinutes >= 60 * 24) {
+                    // 一日前の場合
+                    var days = parseInt(publishedMinutes / (60 * 24));
+                    element.publishStartDate = days + "日前";
+                } else if (publishedMinutes >= 60) {
+                    // 一時間後、一日以内
+                    var hours = parseInt(publishedMinutes / 60);
+                    element.publishStartDate = hours + "時前";
+                } else {
+                    //　一時間以内
+                    element.publishStartDate = publishedMinutes + "分前";
+                }
+            }, this);
             this.communityListForTop = data;
         });
+        
     }
 
     openDetail(community) {
