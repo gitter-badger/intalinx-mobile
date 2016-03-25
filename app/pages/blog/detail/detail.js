9f2d26342676c596e84dc29afe60de9ce5ae7c96
wangtiantian2016 <tiantian.oh@afroci.com>
2016-03-25 17:54:12 +0900

    1.display more when pulling up (blog index / blog detail) 2.refresh when pulling down (blog index)

--- app/pages/blog/detail/detail.js
+++ app/pages/blog/detail/detail.js
@@ -4,7 +4,7 @@ import {Component} from 'angular2/core';
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
 
 import {BlogService} from '../../../providers/blog/blog-service/blog-service';
-import {AddCommentPage} from '../add-comment/add-comment'; 
+import {AddCommentPage} from '../add-comment/add-comment';
 
 /*
   Generated class for the DetailPage page.
@@ -28,7 +28,7 @@ export class DetailPage {
         this.params = params;
         this.id = this.params.get("id");
         this.blogService = blogService;
-        
+
         // this.blog = new Blog("", "お待ちください。");
         this.blogService.getCommunityDetailByCommunityID(this.id).then(data => {
             this.title = data.CommunityOutput.title;
@@ -36,17 +36,211 @@ export class DetailPage {
             this.createDate = data.CommunityOutput.createDate;//.substring(0, 7);
         });
 
-        this.blogService.getReplyContentListByCommunityID(this.id).then(data => {
-            this.comments = data;
-            this.commentCount = data.length;
-        });
+        // this.blogService.getReplyContentListByCommunityID(this.id).then(data => {
+        //     this.comments = data;
+        //     this.commentCount = data.length;
+        // });
+        this.commentListForBolgAll = this.initData();
+
+        // 初期表示のコメント数
+        var firstDisplayCommentCount = 3;
+        // プルアップする時、追加表示のコメント数
+        this.secondAddDisplayCommentCount = 3;
+
+        this.comments = [];
+        this.firstDisplayCount = this.commentListForBolgAll.length;
+
+        this.leftDisplayCount = 0;
+        if (this.commentListForBolgAll.length > firstDisplayCommentCount) {
+            this.firstDisplayCount = firstDisplayCommentCount;
+            this.leftDisplayCount = this.commentListForBolgAll.length - firstDisplayCommentCount;
+        }
+        for (var i = 0; i < this.firstDisplayCount; i++) {
+            this.comments.push(this.commentListForBolgAll[i]);
+        }
+        this.displayedCount = this.firstDisplayCount;
     }
-    
+
+    doInfinite(infiniteScroll) {
+        setTimeout(() => {
+            var nextDisplayCount = this.secondAddDisplayCommentCount;
+            if (this.leftDisplayCount < this.secondAddDisplayCommentCount) {
+                nextDisplayCount = this.leftDisplayCount;
+            }
+            for (var i = 0; i < nextDisplayCount; i++) {
+                this.comments.push(this.commentListForBolgAll[this.displayedCount]);
+                this.leftDisplayCount--;
+                this.displayedCount++;
+            }
+
+            infiniteScroll.complete();
+        }, 500);
+    }
+
     addComment() {
         this.nav.push(AddCommentPage, {
             "id": this.id,
             "title": this.title
         });
+    }
+
+    initData() {
+        return [
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "b69d06be-605a-43f0-b039-e707c1ff0edb",
+                            "content": "ありがとうございます。<br />勉強になります。",
+                            "createDate": "2016/02/15 07:53:17",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "56",
+                            "userName": "丸尾　恭介"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "14cb5d8d-44e5-481c-9e5b-5a0cf71c8823",
+                            "content": "<font size=\"6\">「<span style=\"color: #252525; font-family: 'ＭＳ 明朝', serif; font-size: 15.3333px;\"><font size=\"3\">人間関係の重要さを見くびってはならない</font>。</span>」</font><br />この言葉が大事ですね。<br />正しい事を言っているから大丈夫と勘違いする方々が多いようです。<br /><br />",
+                            "createDate": "2016/02/15 08:35:24",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "2",
+                            "userName": "譚　玉蜂"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee1",
+                            "content": "1多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝1"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee2",
+                            "content": "2多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝2"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee3",
+                            "content": "3多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝3"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee4",
+                            "content": "4多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝4"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee5",
+                            "content": "5多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝5"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee6",
+                            "content": "6多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝6"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee7",
+                            "content": "7多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝7"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee8",
+                            "content": "8多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝8"
+                        }
+                    }
+                }
+            },
+            {
+                "tuple": {
+                    "old": {
+                        "ReplyContentOutput": {
+                            "replyContentID": "eb5818e1-640b-4307-acf8-a6ab6304ceee9",
+                            "content": "9多様な社会では答えは一つではなくなり、それぞれが答えを持つようになります。<br />従って答えはそれぞれが考えねばならない事になり、それぞれの考えをよく聞く事が大切になります。<br />（なにかで読んだ内容です）",
+                            "createDate": "2016/02/15 17:17:32",
+                            "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                            "userID": "1002",
+                            "userName": "田　志輝9"
+                        }
+                    }
+                }
+            }
+        ];
     }
 }
 
--- app/pages/blog/index/index.js
+++ app/pages/blog/index/index.js
@@ -2,7 +2,7 @@ import {Page, NavController} from 'ionic-angular';
 
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
 
-import {BlogService} from '../../../providers/blog/blog-service/blog-service'; 
+import {BlogService} from '../../../providers/blog/blog-service/blog-service';
 
 import {DetailPage} from '../detail/detail';
 
@@ -25,18 +25,256 @@ export class BlogIndexPage {
     constructor(nav, blogService) {
         this.nav = nav;
         this.blogService = blogService;
-        
-        // this.blogService.list().then(data => {
-        //     this.blogs = data;
+
+        // this.communityListForTopAll = [];
+        // this.blogService.getCommunityListForTop().then(data => {
+        //     this.communityListForTopAll = data;
         // });
-        this.blogService.getCommunityListForTop().then(data => {
-            this.communityListForTop = data;
-        });
+        this.communityListForTopAll = this.initData();
+        
+        // 初期表示のブログ数
+        var firstDisplayCommunityCount = 10;
+        // プルアップする時、追加表示のブログ数
+        this.secondAddDisplayCommunityCount = 3;
+        
+        this.communityListForTop = [];
+        this.firstDisplayCount = this.communityListForTopAll.length;
+
+        this.leftDisplayCount = 0;
+        if (this.communityListForTopAll.length > firstDisplayCommunityCount) {
+            this.firstDisplayCount = firstDisplayCommunityCount;
+            this.leftDisplayCount = this.communityListForTopAll.length - firstDisplayCommunityCount;
+        }
+        for (var i = 0; i < this.firstDisplayCount; i++) {
+            this.communityListForTop.push(this.communityListForTopAll[i]);
+        }
+        this.displayedCount = this.firstDisplayCount;
+
     }
 
     openDetail(community) {
         this.nav.push(DetailPage, {
             "id": community.communityID
         });
+    }
+
+    doRefresh(refresher) {
+
+        setTimeout(() => {          
+            // var object = new BlogIndexPage(this.nav, this.blogService);
+            // console.log(object);
+            // this.communityListForTop = object.communityListForTop;
+            this.constructor(this.nav, this.blogService);
+            refresher.complete();
+        }, 5000);
+    }
+    
+    doInfinite(infiniteScroll) {
+        setTimeout(() => {
+            var nextDisplayCount = this.secondAddDisplayCommunityCount;
+            if (this.leftDisplayCount < this.secondAddDisplayCommunityCount) {
+                nextDisplayCount = this.leftDisplayCount;
+            }
+            for (var i = 0; i < nextDisplayCount; i++) {
+                this.communityListForTop.push(this.communityListForTopAll[this.displayedCount]);
+                this.leftDisplayCount--;
+                this.displayedCount++;
+            }
+
+            infiniteScroll.complete();
+        }, 500);
+    }
+    
+    initData() {
+        return [
+            {
+                "communityID": "833e705a-11c4-4f2d-9b0f-2097579f7914",
+                "title": "1タイトルcommunity0224",
+                "createUser": "",
+                "publishStartDate": "2016-02-23T19:14:52.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "5f90c3e5-febe-4cce-949b-4766b0778443",
+                "title": "2AED講習2回目（リスケ分）",
+                "createUser": "",
+                "publishStartDate": "2016-02-16T16:08:41.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a5f96af3-3a40-4f2e-9f1b-62f92762fd11",
+                "title": "3【4F朝会】ビジネスマナーの基本-挨拶（2/15）",
+                "createUser": "",
+                "publishStartDate": "2016-02-16T14:21:56.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "4第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "5第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "6第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "7第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "8第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "9第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "10第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "11第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "12第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "13第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "14第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "15第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "16第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            },
+            {
+                "communityID": "a92b4597-d8e5-431b-bf85-41d9f289044d",
+                "title": "17第５回教育の為のＴＯＣシンポジウム",
+                "createUser": "",
+                "publishStartDate": "2016-02-14T20:32:20.0",
+                "readStatus": "NOT_READ",
+                "readStatusOrder": "1",
+                "newReplyFlag": "FALSE",
+                "newReplyFlagOrder": "0",
+                "isAlreadyExists": "TRUE"
+            }
+            
+        ];
     }
 }
