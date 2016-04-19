cc5b9ef3c85202c769f91ee95c93bc68d432f442
wangtiantian2016 <tiantian.oh@afroci.com>
2016-04-19 15:51:45 +0900

    delete useless code

--- app/providers/blog/blog-service/blog-service.js
+++ app/providers/blog/blog-service/blog-service.js
@@ -27,28 +27,6 @@ export class BlogService {
         this.util = util;
     }
 
-    list() {
-        if (this.data) {
-            // already loaded data
-            return Promise.resolve(this.data);
-        }
-
-        // don't have the data yet
-        return new Promise(resolve => {
-            // We're using Angular Http provider to request the data,
-            // then on the response it'll map the JSON data to a parsed JS object.
-            // Next we process the data and resolve the promise with the new data.
-            this.http.get('./mocks/blogservice/list.json')
-                .map(res => res.json())
-                .subscribe(data => {
-                    // we've got back the raw data, now generate the core schedule data
-                    // and save the data for later reference
-                    this.data = data;
-                    resolve(this.data);
-                });
-        });
-    }
-
     // トップ画面について、ブログリストを取得します
     getCommunityListForTop(position, isNeedRegistNotExistsReply) {
         let rowsPerpage = 10;
