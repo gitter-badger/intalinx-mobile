c071bbf767a46c18b2ca8b8cfb667a2f8177dada
Zhihui Tian <tian.zhihui@intasect.co.jp>
2016-06-19 18:43:09 +0900

    fix bug

--- app/pages/about/about.js
+++ app/pages/about/about.js
@@ -33,7 +33,7 @@ export class AboutPage {
         this.version = "latest";
         this.latestVersion = "latest";
         this.upgradeUrl = "latest";
-        //this.getVersionInfo();
+        this.getVersionInfo();
         this.getUpgradeUrl();
     }  
   
