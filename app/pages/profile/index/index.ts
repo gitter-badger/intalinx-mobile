613999d980f490b5be1642bbdd739321876ad625
wincy7 <akane_sisi@yahoo.co.jp>
2016-07-20 17:06:46 +0900

    Translate js to ts.

--- app/pages/profile/index/index.ts
+++ app/pages/profile/index/index.ts
@@ -1,14 +1,22 @@
-import {Page, IonicApp, NavController, ViewController, Platform} from 'ionic-angular';
-
+// Third party library.
+import {Injectable, Component} from '@angular/core';
+import {NavController, ViewController, Platform} from 'ionic-angular';
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
+// Config.
+import {AppConfig} from '../../../appconfig';
 
+// Utils.
+import {Util} from '../../../utils/util';
+
+// Services.
+import {UserService} from '../../../providers/user-service';
+import {ShareService} from '../../../providers/share-service';
+
+// Pages
 import {ChangePasswordPage} from '../change-password/change-password';
 import {ChangeAvatarPage} from '../change-avatar/change-avatar';
 
-import {UserService} from '../../../providers/user-service';
-import {Util} from '../../../utils/util';
-
-@Page({
+@Component({
     templateUrl: 'build/pages/profile/index/index.html',
     providers: [
         UserService,
@@ -16,23 +24,22 @@ import {Util} from '../../../utils/util';
     ],
     pipes: [TranslatePipe]
 })
-
+@Injectable()
 export class ProfileIndexPage {
-    static get parameters() {
-        return [[IonicApp], [NavController], [UserService], [ViewController], [Platform]];
-    }
+    private userDefaultAvatarImageUrl: string;
+    private user: any;
+    private isLoadCompleted: boolean;
 
-    constructor(app, nav, userService, view, platform) {
-        this.app = app;
-        this.nav = nav;
-        this.userService = userService;
-        this.view = view;
-        this.platform = platform;
-
-        this.userDefaultAvatarImageUrl = this.app.config.get("USER_DEFAULT_AVATAR_IMAGE_URL");
+    constructor(private nav: NavController,
+        private view: ViewController,
+        private platform: Platform,
+        private appConfig: AppConfig,
+        private userService: UserService,
+        private share: ShareService) {
+        this.userDefaultAvatarImageUrl = this.appConfig.get('USER_DEFAULT_AVATAR_IMAGE_URL');
 
         this.user = {
-            "userAvatar": this.userDefaultAvatarImageUrl
+            'userAvatar': this.userDefaultAvatarImageUrl
         };
     }
 
@@ -44,9 +51,9 @@ export class ProfileIndexPage {
         });
     }
 
-    openChangeAvatar(user) {
+    openChangeAvatar(user: any) {
         this.nav.push(ChangeAvatarPage, {
-            "user": user
+            'user': user
         });
     }
 
