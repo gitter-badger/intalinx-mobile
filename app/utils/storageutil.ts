ba12e0eaf7f3bc400122ab2ba772afbd04b29184
Zhihui Tian <tian.zhihui@intasect.co.jp>
2016-07-20 11:49:30 +0900

    storageutil.

--- app/utils/storageutil.ts
+++ app/utils/storageutil.ts
@@ -1,39 +1,39 @@
 export class StorageUtil {
-    
+
     constants: any = {
         NOT_ON_OR_AFTER: 'notOnOrAfter'
     };
-    
+
     constructor() {
-        
+
     }
-    
+
     getStorage() {
         return sessionStorage;
     }
-    
+
     setSAMLart(key, value, notOnOrAfter) {
         this.getStorage().setItem(key, value);
         this.getStorage().setItem(this.constants.NOT_ON_OR_AFTER, notOnOrAfter);
     }
-    
+
     getSAMLart(key) {
         if (this.hasSAMLart(key)) {
             return this.getStorage().getItem(key);
         }
     }
-    
+
     getSAMLartExpireDate(key) {
         if (this.getStorage().getItem(key)) {
             return this.getStorage().getItem(this.constants.NOT_ON_OR_AFTER);
         }
     }
-    
+
     removeSAMLart(key) {
         this.getStorage().removeItem(key);
         this.getStorage().removeItem(this.constants.NOT_ON_OR_AFTER);
     }
-    
+
     hasSAMLart(key) {
         let hasSAMLart = true;
         if (this.getSAMLartExpireDate(key) && new Date(this.getSAMLartExpireDate(key)) < new Date()) {
