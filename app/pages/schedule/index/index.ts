198956501caffc463692c00a6523ce22a97eb683
wangtiantian2016 <tiantian.oh@afroci.com>
2016-07-28 18:11:18 +0900

    remove the slide of calendar

--- app/pages/schedule/index/index.ts
+++ app/pages/schedule/index/index.ts
@@ -53,7 +53,7 @@ export class ScheduleIndexPage {
     private cachedSlidesOnOneSide: number = 1;
 
     private calendarSlideOptions: any;
-    private numbers: any;
+    // private numbers: any;
     private isFirstDayMonday: boolean;
     private today: any;
     private yearMonth: any;
@@ -80,7 +80,7 @@ export class ScheduleIndexPage {
             direction: 'vertical',
             initialSlide: this.cachedSlidesOnOneSide
         };
-        this.numbers = this.initNumbers(this.defaultNumber, this.cachedSlidesOnOneSide);
+        // this.numbers = this.initNumbers(this.defaultNumber, this.cachedSlidesOnOneSide);
         this.weekdays = moment.weekdaysMin(false);
         // In Japan,the first day of the week is Monday. In China and England, the first day of the week is Sunday.
         if (this.userLang === 'ja' || this.userLang === 'ja-jp') {
@@ -213,46 +213,47 @@ export class ScheduleIndexPage {
     /**
        * Makes an initial array of numbers to slide, based on the cache size specified
        */
-    initNumbers(defaultNumber: number, cachedSlidesOnOneSide: number): any {
-        let length = 2 * cachedSlidesOnOneSide + 1;
-        let numbers = new Array(length);
-        numbers[cachedSlidesOnOneSide] = defaultNumber;
-        let pushedNumber = defaultNumber;
-        for (let i = cachedSlidesOnOneSide - 1; i >= 0; i--) {
-            pushedNumber--;
-            numbers[i] = pushedNumber;
-        }
-        pushedNumber = defaultNumber;
-        for (let i = cachedSlidesOnOneSide + 1; i < length; i++) {
-            pushedNumber++;
-            numbers[i] = pushedNumber;
-        }
-        return numbers;
-    }
+    // initNumbers(defaultNumber: number, cachedSlidesOnOneSide: number): any {
+    //     let length = 2 * cachedSlidesOnOneSide + 1;
+    //     let numbers = new Array(length);
+    //     numbers[cachedSlidesOnOneSide] = defaultNumber;
+    //     let pushedNumber = defaultNumber;
+    //     for (let i = cachedSlidesOnOneSide - 1; i >= 0; i--) {
+    //         pushedNumber--;
+    //         numbers[i] = pushedNumber;
+    //     }
+    //     pushedNumber = defaultNumber;
+    //     for (let i = cachedSlidesOnOneSide + 1; i < length; i++) {
+    //         pushedNumber++;
+    //         numbers[i] = pushedNumber;
+    //     }
+    //     return numbers;
+    // }
 
-    changeMonth(swiper) {
-        let swipeDirection = swiper.swipeDirection;
-        if (swipeDirection) {
-            let newIndex = this.slider.getActiveIndex();
-            if (swipeDirection === 'prev') {
-                while (newIndex < this.cachedSlidesOnOneSide) {
-                    newIndex++;
-                    this.numbers.unshift(this.numbers[0] - 1);
-                    this.numbers.pop();
-                    this.lastMonth();
-                }
-            } else {
-                while (newIndex > this.cachedSlidesOnOneSide) {
-                    newIndex--;
-                    this.numbers.push(this.numbers[this.numbers.length - 1] + 1);
-                    this.numbers.shift();
-                    this.nextMonth();
-                }
-            }
-            // Workaround to make it work: breaks the animation
-            this.slider.slideTo(newIndex, 0, false);
-        }
-    }
+    // Dont use slide because there is a bug of Slides in ionic version beta10 
+    // changeMonth(swiper) {
+    //     let swipeDirection = swiper.swipeDirection;
+    //     if (swipeDirection) {
+    //         let newIndex = this.slider.getActiveIndex();
+    //         if (swipeDirection === 'prev') {
+    //             while (newIndex < this.cachedSlidesOnOneSide) {
+    //                 newIndex++;
+    //                 this.numbers.unshift(this.numbers[0] - 1);
+    //                 this.numbers.pop();
+    //                 this.lastMonth();
+    //             }
+    //         } else {
+    //             while (newIndex > this.cachedSlidesOnOneSide) {
+    //                 newIndex--;
+    //                 this.numbers.push(this.numbers[this.numbers.length - 1] + 1);
+    //                 this.numbers.shift();
+    //                 this.nextMonth();
+    //             }
+    //         }
+    //         // Workaround to make it work: breaks the animation
+    //         this.slider.slideTo(newIndex, 0, false);
+    //     }
+    // }
 
     openEventDetail(event) {
         this.sendDataToShowOrDeleteEvent.selectedDay = this.selectedDay;
