2741ea6e60140c079d4bd3d8b6fcb678ff64c90b
Zhihui Tian <tian.zhihui@intasect.co.jp>
2016-07-29 18:59:32 +0900

    change year min to 2005 max to 2020.

--- app/appconfig.ts
+++ app/appconfig.ts
@@ -3,8 +3,8 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class AppConfig {
     private config: any = {
-        // 'BASE_URL': 'https://iscsys.intasect.co.jp/home/InternalSystem/',
-        'BASE_URL': 'http://192.168.11.29/home/intalinxcloud/',
+        'BASE_URL': 'https://iscsys-staging.intasect.co.jp/home/InternalSystem/',
+        //'BASE_URL': 'http://192.168.11.29/home/intalinxcloud/',
         'GATEWAY_URL': 'com.eibus.web.soap.Gateway.wcp',
         'PRE_LOGIN_INFO_URL': 'com.eibus.sso.web.authentication.PreLoginInfo.wcp',
         'SAMLART_NAME': 'SAMLart',
@@ -13,13 +13,13 @@ export class AppConfig {
         'AUTO_LOGIN_STORAGE_NAME': 'autoLogin',
         'LOGIN_ID_STORAGE_NAME': 'loginID',
         'PASSWORD_STORAGE_NAME': 'password',
-        USER_DEFAULT_AVATAR_IMAGE_URL: 'img/default',
+        'USER_DEFAULT_AVATAR_IMAGE_URL': 'img/default',
         'PGYER_ANDROID_URL' : 'https://www.pgyer.com/MMHC',
         'PGYER_IOS_URL' : 'https://www.pgyer.com/MMHB',
         'USER_LANG': 'en-US',
         'GOOGLE_ANALYTICS_TRACK_ID': 'UA-81438804-1',
-        'DATETIME_YEAR_MONTH_DAY_MIN': '2004-01-01',
-        'DATETIME_YEAR_MONTH_DAY_MAX': '2080-12-31',
+        'DATETIME_YEAR_MONTH_DAY_MIN': '2005-01-01',
+        'DATETIME_YEAR_MONTH_DAY_MAX': '2020-12-31',
         'DATETIME_MINUTE_VALUES': '00, 15, 30, 45'
     };
     get(key: string): string {
