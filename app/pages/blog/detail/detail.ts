826559042d5dacc8867a35bde61f5ca78adf754f
wangtt2016@gmail.com <wangtt2016@gmail.com>
2016-08-22 14:03:17 +0900

    add save picture by wangtt

--- app/pages/blog/detail/detail.ts
+++ app/pages/blog/detail/detail.ts
@@ -1,7 +1,7 @@
 // Third party library.
 import {Component, ViewChild, ViewChildren, ContentChildren, EventEmitter, Injectable, Directive, HostListener, ViewContainerRef, ElementRef, Renderer, Input, QueryList, DynamicComponentLoader} from '@angular/core';
-import {NavController, NavParams, Content, Slides} from 'ionic-angular';
-import {Transfer} from 'ionic-native';
+import {NavController, NavParams, Content, Slides, ActionSheetController, ToastController} from 'ionic-angular';
+import {Base64ToGallery} from 'ionic-native';
 import {TranslateService} from 'ng2-translate/ng2-translate';
 
 // Utils.
@@ -221,7 +221,7 @@ export class BlogDetailPage {
     template: `
     <ion-content class="image-slides">
         <ion-slides [options]="imageSlideOptions">
-            <ion-slide *ngFor="let image of images" (click)="backToBlogDetail()">
+            <ion-slide *ngFor="let image of images" (click)="backToBlogDetail()" (press)="showPictureOperations(image.src)">
                 <img src="{{image.src}}" />
             </ion-slide>
         </ion-slides>
@@ -232,7 +232,7 @@ class ImageSlidesPage {
     private sendData: any;
     private images: any;
     private imageSlideOptions: any;
-    constructor(private nav: NavController, private params: NavParams) {
+    constructor(private nav: NavController, private actionSheetCtrl: ActionSheetController, private toastCtrl: ToastController, private params: NavParams, private util: Util, private translate: TranslateService) {
         this.sendData = this.params.get('sendData');
         this.images = Array.prototype.slice.call(this.sendData.images);
         let currentImage = this.sendData.currentImage;
@@ -251,5 +251,56 @@ class ImageSlidesPage {
 
     backToBlogDetail() {
         this.nav.pop();
+    }
+
+    showPictureOperations(base64Data) {
+        this.translate.get(['app.action.savePicture',
+            'app.action.cancel']).subscribe(message => {
+                let deleteEventOfSelectedDay = message['app.action.savePicture'];
+                let cancelButton = message['app.action.cancel'];
+                let actionSheet = this.actionSheetCtrl.create({
+                    buttons: [
+                        {
+                            text: deleteEventOfSelectedDay,
+                            handler: () => {
+                                this.savePicture(base64Data);
+                            }
+                        }, {
+                            text: cancelButton,
+                            handler: () => {
+
+                            }
+                        }
+                    ]
+                });
+                actionSheet.present();
+            });
+
+    }
+
+    savePicture(base64Data) {
+        Base64ToGallery.base64ToGallery(base64Data, 'img_').then(
+            res => (setTimeout(() => {this.showSuccessToast()}, 500)),
+            err => this.showErrorPresent()
+        );
+
+    }
+
+    showSuccessToast() {
+        this.translate.get('app.blog.message.success.savePicture').subscribe(message => {
+            let content = message;
+            let toast = this.toastCtrl.create({
+                message: content,
+                duration: 3000,
+                cssClass: 'middle'
+            });
+            toast.present();
+        });
+    }
+
+    showErrorPresent() {
+        this.translate.get('app.blog.message.error.savePicture').subscribe(message => {
+            this.util.presentModal(message);
+        });
     }
 }
