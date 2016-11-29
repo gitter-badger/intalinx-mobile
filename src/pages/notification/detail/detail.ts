9b510726fa89348ecdf17e05def35dbbf7c21a6e
wangtt <wangtt2016@gmail.com>
2016-11-29 17:32:16 +0900

    upgrade to ionic 2.0.0-rc.3

---
+++ src/pages/notification/detail/detail.ts
@@ -0,0 +1,156 @@
+// Third party library.
+import {Component, ViewChild,  Directive, HostListener, ViewContainerRef} from '@angular/core';
+import {NavController, NavParams, ViewController, Content} from 'ionic-angular';
+import {NotificationService} from '../../../providers/notification-service';
+
+// Utils.
+import {Util} from '../../../utils/util';
+
+// Services.
+import {ShareService} from '../../../providers/share-service';
+
+// Pages.
+// import {InnerContent} from '../../../shared/components/innercontent/innercontent';
+import {ImageSlidesPage} from '../../../shared/components/image-slides/image-slides';
+
+@Directive({
+    selector: 'img'
+})
+export class Img {
+    private images: any;
+    constructor(private nav: NavController, private elementRef: ViewContainerRef) {
+    }
+    @HostListener('click', [])
+    onClick() {
+        let currentImage = this.elementRef.element.nativeElement;
+        if (currentImage.parentElement.parentElement.className === 'contents selectable') {
+            let images = currentImage.ownerDocument.querySelectorAll('.contents img');
+            let sendData = {
+                'currentImage': currentImage,
+                'images': images
+            };
+            this.nav.push(ImageSlidesPage, { 'sendData': sendData });
+        }
+    }
+}
+
+@Component({
+    selector: 'page-notification-detail',
+    templateUrl: 'detail.html',
+    providers: [
+        NotificationService,
+        Util
+    ]
+    // ,
+    // directives: [InnerContent]
+})
+
+export class NotificationDetailPage {
+    @ViewChild(Content) pageContent: Content;
+
+    private notification: any;
+    private id: string;
+    private readStatus: string;
+    // The detail data of event.
+    private title: string;
+    private content: any;
+    private createUserId: string;
+    private publishStartDate: string;
+    private createUserAvatar: string;
+    private createUserName: string;
+    private status: string;
+    private readCount: string;
+    private isLoadCompleted: boolean;
+    private isScrollToTopButtonVisible: boolean;
+    // The number of milliseconds between midnight, January 1, 1970.
+    private pageLoadTime: number;
+    private attachFilesForDownload: any;
+    private attachImagesForDisplay: any;
+    private hasAttachFilesForDownload: boolean = false;
+
+    constructor(private nav: NavController,
+        private params: NavParams,
+        private notificationService: NotificationService,
+        private view: ViewController,
+        private share: ShareService) {
+        this.notification = this.params.get('notification');
+        this.id = this.notification.notificationID;
+        this.readStatus = this.notification.readStatus;
+
+        this.getNotificationDetailByNotificationID();
+    }
+
+    getNotificationDetailByNotificationID(): void {
+        this.notificationService.getNotificationDetailByNotificationID(this.id).then((data: any) => {
+            this.title = data.title;
+            this.content = [data.content, [Img]];
+            this.createUserId = data.createUser;
+            this.publishStartDate = data.publishStartDate;
+            this.createUserAvatar = data.createUserAvatar;
+            this.createUserName = data.createUserName;
+            this.status = data.status;
+            this.readCount = data.readCount;
+            this.attachImagesForDisplay = data.attachImagesForDisplay;
+            this.attachFilesForDownload = data.attachFilesForDownload;
+            if (data.attachFilesForDownload.length > 0) {
+                this.hasAttachFilesForDownload = true;
+            }
+            this.isLoadCompleted = true;
+            this.isScrollToTopButtonVisible = false;
+
+        });
+    }
+
+    ionViewDidLoad(): void {
+        this.pageLoadTime = new Date().getTime();
+    }
+
+    ionViewWillUnload(): void {
+        let now = new Date().getTime();
+        let pageLoadingTime = now - this.pageLoadTime;
+        if (this.status === 'PUBLISH' && this.readStatus === 'NOT_READ' && pageLoadingTime >= 3000) {
+            this.updateReadStatus();
+        }
+        this.isLoadCompleted = false;
+        this.isScrollToTopButtonVisible = false;
+    }
+
+    updateReadStatus(): void {
+        let readStatus = 'READ';
+        this.notificationService.updateReadStatus(this.id, readStatus).then((data: string) => {
+            if (data === 'true') {
+                this.notification.readStatus = readStatus;
+                let notificationNewInformationCount = Number(this.share.notificationNewInformationCount);
+                this.share.notificationNewInformationCount = notificationNewInformationCount - 1;
+            }
+        });
+    }
+
+    ngAfterViewInit(): void {
+        this.pageContent.addScrollListener(this.onPageScroll(this));
+    }
+
+    scrollToDetailPageTop(): void {
+        this.pageContent.scrollToTop();
+    }
+
+    onPageScroll(that): any {
+        return function () {
+            if (this.scrollTop > 200) {
+                that.isScrollToTopButtonVisible = true;
+            } else {
+                that.isScrollToTopButtonVisible = false;
+            }
+        };
+    }
+
+    showImageSlides(event): any {
+        let currentImage = event.currentTarget;
+        let images = document.querySelectorAll('.contents img');
+        let sendData = {
+            'currentImage': currentImage,
+            'images': images
+        };
+        this.nav.push(ImageSlidesPage, { 'sendData': sendData });
+    }
+}
---
+++ src/pages/portal/portal.ts
@@ -0,0 +1,160 @@
+// Third party library.
+import {Component} from '@angular/core';
+import {TranslateService} from 'ng2-translate/ng2-translate';
+import {Platform, NavController, MenuController} from 'ionic-angular';
+import {InAppBrowser} from 'ionic-native';
+
+// Utils.
+import {Util} from '../../utils/util';
+
+// Config.
+import {AppConfig} from '../../app/app.config';
+
+// Services.
+import {ShareService} from '../../providers/share-service';
+import {AppsService} from '../../providers/apps-service'; 
+import {UserService} from '../../providers/user-service';
+import {BlogService} from '../../providers/blog-service'; 
+import {NotificationService} from '../../providers/notification-service';
+import {ScheduleService} from '../../providers/schedule-service';
+import {AboutService} from '../../providers/about-service';
+
+import {InfoPage} from '../info/info';
+import {BlogIndexPage} from '../blog/index/index';
+import {ProfileIndexPage} from '../profile/index/index';
+import {NotificationIndexPage} from '../notification/index/index';
+import {SurveyIndexPage} from '../survey/index/index';
+import {ScheduleIndexPage} from '../schedule/index/index';
+import {AboutPage} from '../about/about';
+import {DevicesPage} from '../schedule/devices/devices';
+
+@Component({
+    selector: 'page-portal',
+    templateUrl: 'portal.html',
+    providers: [
+        AppsService,
+        UserService,
+        BlogService,
+        NotificationService,
+        ScheduleService,
+        AboutService
+    ]
+})
+
+export class PortalPage {
+    version: string = 'latest';
+    latestVersion: string = 'latest';
+
+    components = {
+        'portal': PortalPage,
+        'info' : InfoPage,
+        'profile' : ProfileIndexPage,
+        'schedule': ScheduleIndexPage,
+        'about': AboutPage,
+        'devices': DevicesPage
+    };
+
+    constructor(private translate: TranslateService, private platform: Platform, private nav: NavController, private appConfig: AppConfig, private util: Util, private share: ShareService, private appsService: AppsService, private aboutService: AboutService, private userService: UserService) {
+        this.initializeUser().then(() => {
+            this.loadApplications();
+            this.checkUpdate();
+        });        
+        if (!this.share.showMenu) {
+            this.share.showMenu = this.showMenu(this);
+        }
+    }
+
+    checkUpdate(): void {
+        // check latest version from http://pgyer.com/.
+        if (this.platform.is('cordova') && !this.appConfig.get('IS_TABLET')) {
+            this.aboutService.getVersion().then(data => {
+                this.version = data;
+                this.aboutService.getLatestVersion().then(data => {
+                    this.latestVersion = data;
+                    if (this.latestVersion !== this.version) {
+                        this.translate.get(['app.message.info.versionTooOld']).subscribe(message => {
+                            this.util.presentModal(message['app.message.info.versionTooOld'], 'info');
+                        });
+                    }
+                });
+            });
+        }
+    }
+
+    loadApplications() {
+        return this.appsService.load().then((data: any) => {
+            let menuIdNeedToRemove = [];
+            
+            // remove about page for web browser.
+            if (!this.platform.is('cordova')) {
+                menuIdNeedToRemove.push('about');
+            }
+            // remove notification, calendar, profile for real device.
+            if (this.appConfig.get('IS_TABLET')) {
+                menuIdNeedToRemove.push('blog');
+                menuIdNeedToRemove.push('notification');
+		menuIdNeedToRemove.push('survey');
+                menuIdNeedToRemove.push('schedule');
+                menuIdNeedToRemove.push('profile');
+                menuIdNeedToRemove.push('about');
+            } else if (this.platform.is('mobile')) {
+                menuIdNeedToRemove.push('devices');
+            }
+            // remove unnecessary menu.
+            for (let index = 0; index < data.length; index ++) {
+                let currentValue = data[index];
+                for (let i = 0; i < menuIdNeedToRemove.length; i++) {
+                    if (currentValue.componentsId === menuIdNeedToRemove[i]) {
+                        data.splice(index, 1);
+                        menuIdNeedToRemove.splice(i, 1);
+                        index--;
+                        break;
+                    }
+                }
+            }
+
+            this.share.initializeMenu(data);
+
+            // set root page.
+            if (!this.appConfig.get('IS_TABLET')) {
+                // set root to blog.
+                this.nav.setRoot(InfoPage);
+            } else {
+                // set root to devices on tablet
+                this.nav.setRoot(DevicesPage);
+            }
+        });
+    }
+
+    initializeUser() {
+        return new Promise(resolve => {
+            this.userService.getUserDetails().then(data => {
+                this.share.initializeUser(data);
+                resolve();
+            });
+        });
+    }
+  
+    showMenu(that) {
+        return function(menu) {
+            if (menu.componentsId === 'biznavi') {
+                that.util.getSAMLart().then((samlart: string) => {
+                    // sso for biznavi.
+                    let baseURL = that.appConfig.get('BASE_URL');
+                    let baseURLChina = that.appConfig.get('BASE_URL_CHINA');
+                    let url = that.appConfig.get('BIZNAVI_URL_JAPAN') + samlart;
+                    if (baseURL === baseURLChina) {
+                        url = that.appConfig.get('BIZNAVI_URL_CHINA') + samlart;
+                    }
+                    if (that.platform.is('cordova')) {
+                        InAppBrowser.open(url, '_system');
+                    } else {
+                        window.open(url, '_blank');
+                    }
+                });
+            } else {
+                that.nav.setRoot(that.components[menu.componentsId]);
+            }
+        };
+    }
+}
